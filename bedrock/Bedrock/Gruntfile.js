51fca3552bd75fab73addf316e3bbdc729b600dd
FranklinSilva <franklinsilva@outlook.com>
2015-08-23 00:51:45 -0300

    first version bedrock docs website

---
+++ bedrock/Bedrock/Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+
+    serve: {
+      options: {
+        port: 9000
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-serve');
+
+  grunt.registerTask( 'default', [ 'serve' ] );  
+
+};
