5592584b51bb2d5233c3ff91f6ba1d6be8dbea42
Franklin Silva <franklinsilva@outlook.com>
2018-09-03 23:55:46 -0300

    desisti scroll por agora

--- loveplaces/js/script.js
+++ loveplaces/js/script.js
@@ -1,22 +1,8 @@
-function onScrollInit( items, trigger ) {
-  items.each( function() {
-    var osElement = $(this),
-        osAnimationClass = osElement.attr('data-os-animation'),
-        osAnimationDelay = osElement.attr('data-os-animation-delay');
- 
-    osElement.css({
-        '-webkit-animation-delay':  osAnimationDelay,
-        '-moz-animation-delay':     osAnimationDelay,
-        'animation-delay':          osAnimationDelay
+var scrollTo = function() {
+    console.log("oi oi oi");
+    var el = document.querySelector('#newsletter');
+    console.log(el);
+    el.scrollIntoView({
+        behavior: 'smooth'
     });
- 
-    var osTrigger = ( trigger ) ? trigger : osElement;
- 
-    osTrigger.waypoint(function() {
-        osElement.addClass('animated').addClass(osAnimationClass);
-    },{
-        triggerOnce: true,
-        offset: '90%'
-    });
-  });
 }
